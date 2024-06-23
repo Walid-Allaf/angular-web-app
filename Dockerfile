b54d0841c28b0aff41c845e5ebe3b9f02a7321d4
Walid-Allaf <hammoda3165@gmail.com>
2024-06-24 00:48:05 +0300

    init commit for angular app

---
+++ Dockerfile
@@ -0,0 +1,15 @@
+FROM node:alpine AS angular
+
+WORKDIR /app
+
+COPY . .    
+RUN npm install
+RUN npm run build          
+
+FROM nginx:alpine
+
+COPY --from=angular /app/dist/dockerExample /usr/share/nginx/html
+
+EXPOSE 80
+
+CMD ["nginx", "-g", "daemon off;"]
---
+++ src/environments/environment.ts
@@ -0,0 +1,26 @@
+import { Environment } from '@abp/ng.core';
+
+const baseUrl = 'http://localhost:4200';
+
+export const environment = {
+  production: false,
+  application: {
+    baseUrl,
+    name: 'dockerExample',
+    logoUrl: '',
+  },
+  oAuthConfig: {
+    issuer: 'https://localhost:44311/',
+    redirectUri: baseUrl,
+    clientId: 'dockerExample_App',
+    responseType: 'code',
+    scope: 'offline_access dockerExample',
+    requireHttps: true,
+  },
+  apis: {
+    default: {
+      url: 'https://localhost:44311',
+      rootNamespace: 'dockerExample',
+    },
+  },
+} as Environment;
